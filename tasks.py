e26637ec2e61b47f76faf883b585dde6fc5c6a30
Rajeev J Sebastian <rajeev@alokin.in>
2021-03-14 20:57:39 +0530

    add requests to Pipenv; minor changes; fix indentation

--- tasks.py
+++ tasks.py
@@ -3,17 +3,19 @@ import requests
 import json
 import subprocess
 
+API_ROOT = "https://api.atfg.gtechindia.org/"
+
 @task
 def build(c):
-  #fetch student and college data from api
-  colleges = requests.get("https://api.atfg.gtechindia.org/college").json()
-  students = requests.get("https://api.atfg.gtechindia.org/students").json()
+    # fetch student and college data from api
+    colleges = requests.get(API_ROOT+"colleges").json()
+    students = requests.get(API_ROOT+"students").json()
 
-  #write to json files in data directory
-  with open('data/colleges.json', 'w') as outfile:
-    json.dump(colleges, outfile)
-  with open('data/students.json', 'w') as outfile:
-    json.dump(students, outfile)
+    # write to json files in data directory
+    with open('data/colleges.json', 'w') as outfile:
+        json.dump(colleges, outfile)
+    with open('data/students.json', 'w') as outfile:
+        json.dump(students, outfile)
 
-  #invoke render-hugo buildcommand
-  subprocess.run("hugo --gc --minify", shell=True)
+    # invoke render-hugo buildcommand
+    subprocess.run("hugo --gc --minify", shell=True)
