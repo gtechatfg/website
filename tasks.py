ef6f1425dd13ca829af48d56d0a3af83e8f458b9
Rajeev J Sebastian <rajeev@alokin.in>
2021-06-07 10:02:15 +0530

    Create colleges/students JSON file if it doesnt exist

--- tasks.py
+++ tasks.py
@@ -12,9 +12,9 @@ def build(c):
     students = requests.get(API_ROOT+"students").json()
 
     # write to json files in data directory
-    with open('data/colleges.json', 'w') as outfile:
+    with open('data/colleges.json', 'w+') as outfile:
         json.dump(colleges, outfile)
-    with open('data/students.json', 'w') as outfile:
+    with open('data/students.json', 'w+') as outfile:
         json.dump(students, outfile)
 
     # invoke render-hugo buildcommand
