3fc81d94dab44fbe7bb7bf08003e74eabdb9a267
Rajeev J Sebastian <rajeev@alokin.in>
2021-06-07 10:12:38 +0530

    fix build issues

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ from invoke import task
 import requests
 import json
 import subprocess
+import os
 
 API_ROOT = "https://api.atfg.gtechindia.org/"
 
@@ -10,6 +11,10 @@ def build(c):
     # fetch student and college data from api
     colleges = requests.get(API_ROOT+"college").json()
     students = requests.get(API_ROOT+"students").json()
+
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
 
     # write to json files in data directory
     with open('data/colleges.json', 'w+') as outfile:
